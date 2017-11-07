f5c825c6b65faebc713db55c7bccbec13d6dd1c1
if1live <libsora25@gmail.com>
2017-11-07 15:18:21 +0900

    test on unity 2017.2.0p1

---
+++ Assets/ChihoExamples/NativeRecenterExample.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Assets.ChihoExamples {
+    class NativeRecenterExample : MonoBehaviour {
+        const KeyCode key_recenter = KeyCode.R;
+
+        void Start() {
+            var ok = XRDevice.SetTrackingSpaceType(UnityEngine.XR.TrackingSpaceType.Stationary);
+            Debug.LogFormat("TrackingSpace : {0}, {1}", XRDevice.GetTrackingSpaceType(), ok);
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(key_recenter)) {
+                Recenter();   
+            }
+        }
+
+        internal void Recenter() {
+            InputTracking.Recenter();
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(NativeRecenterExample))]
+    class NativeRecenterExampleEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            var script = target as NativeRecenterExample;
+            if (GUILayout.Button("Recenter")) {
+                script.Recenter();
+            }
+        }
+    }
+#endif
+}
